7e369f18aab5fe31ad9de2f39754ab781f4a9cdf
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-07-25 10:02:13 -0300

    [feat] Doing Clean Arch 2

--- lib/app/presenter/ui/brand_page.dart
+++ lib/app/presenter/ui/brand_page.dart
@@ -1,9 +1,10 @@
+import 'dart:ui';
+
 import '../../data_or_infra/datasources/local/get_computer_bybrand_local_datasource.dart';
 import '../../data_or_infra/repositories/get_computer_bybrand_repository_imp.dart';
 import '../../domain/usecases/get_computer_bybrand/get_computer_bybrand_usecase_imp.dart';
 import '../controllers/brand_controller.dart';
 import 'package:flutter/material.dart';
-
 
 class BrandPage extends StatefulWidget {
   const BrandPage({Key? key}) : super(key: key);
@@ -24,9 +25,18 @@ class _BrandPageState extends State<BrandPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.grey,
-        child: Text(brandController.brand.brand),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.grey,
+            child: Text(
+              brandController.brand.model,
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
       ),
     );
   }
